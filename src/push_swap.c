1186fe0dfcb633f7cfcebb392734606dd7efb2f9
Aurelien Girona <agirona@z3r12p3.42lyon.fr>
2021-09-15 20:01:45 +0200

    push sorted value back in A

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: agirona <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 02:25:36 by agirona           #+#    #+#             */
-/*   Updated: 2021/09/14 19:42:31 by agirona          ###   ########lyon.fr   */
+/*   Updated: 2021/09/15 19:50:45 by agirona          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,15 +111,24 @@ void	rush_b(t_stack *a_stack, t_stack *b_stack)
 {
 	int			i;
 	float		percent;
+	int			len;
 	t_element	*current;
+	int			pivot2;
 
-	percent = 0.13125;
+	if (a_stack->len <= 100)
+		percent = 0.13125 * 3;
+	else
+		percent = 0.13125;
 	while (a_stack->len > 2)
 	{
+		len = a_stack->len;
 		a_stack->position = a_stack->len * percent;
 		a_stack->pivot = select_pivot(a_stack, a_stack->position);
-		ft_putstr("pivot = ");
+		pivot2 = select_pivot(a_stack, a_stack->position * 0.5);
+		ft_putstr("pivot1 = ");
 		ft_putnbr(a_stack->pivot);
+		ft_putstr(" pivot2 = ");
+		ft_putnbr(a_stack->position / 2);
 		ft_putchar('\n');
 		i = 0;
 		while (i < a_stack->position && a_stack->position != 1)
@@ -128,12 +137,94 @@ void	rush_b(t_stack *a_stack, t_stack *b_stack)
 			if (current->value <= a_stack->pivot)
 			{
 				push(a_stack, b_stack);
+				if (b_stack->first->value < pivot2)
+					rotate(b_stack);
 				i++;
 			}
 			else
 				rotate(a_stack);
 		}
-		percent += 0.425;
+		if (percent < 0.70)
+			percent += 0.195;
+		print_data(a_stack, b_stack);
+	}
+}
+
+int		get_next_highter_value(t_stack *stack, int value)
+{
+	int			find;
+	int			position;
+	int			i;
+	t_element	*current;
+
+	find = 0;
+	position = 0;
+	i = 1;
+	current = stack->first;
+	while (current != NULL)
+	{
+		if (current->value < value)
+		{
+			find = current->value;
+			current = NULL;
+			position = i;
+		}
+		else
+			current = current->next;
+		i++;
+	}
+	current = stack->first;
+	i = 1;
+	while (current != NULL)
+	{
+		if (current->value > find && current->value < value)
+		{
+			find = current->value;
+			position = i;
+		}
+		current = current->next;
+		i++;
+	}
+	print_stack(stack);
+	ft_putstr("valeur envoye = ");
+	ft_putnbr(value);
+	ft_putstr(" position = ");
+	ft_putnbr(position);
+	ft_putstr(" valeur = ");
+	ft_putnbr(find);
+	return (position);
+}
+
+void	counter_b(t_stack *a_stack, t_stack *b_stack)
+{
+	int			position;
+	int			i;
+
+	while (b_stack->len != 0)
+	{
+		position = get_next_highter_value(b_stack, a_stack->first->value);
+		if (position < b_stack->len * 0.5)
+		{
+			i = 1;
+			while (i < position)
+			{
+				rotate(b_stack);
+				i++;
+			}
+		}
+		else
+		{
+			i = b_stack->len + 1;
+			while (i > position)
+			{
+				reverse_rotate(b_stack);
+				i--;
+			}
+		}
+		ft_putstr("valeur push = ");
+		ft_putnbr(b_stack->first->value);
+		ft_putchar('\n');
+		push(b_stack, a_stack);
 	}
 }
 
@@ -155,10 +246,12 @@ void	create_stack(int argc, char **argv)
 
 	print_data(a_stack, b_stack);
 	ft_putchar('\n');
+	//check if A is sorted
 	rush_b(a_stack, b_stack);
+	counter_b(a_stack, b_stack);
+	ft_putchar('\n');
+	ft_putchar('\n');
 	print_data(a_stack, b_stack);
-
-
 
 
 	delete_stack(a_stack, b_stack);
